8a9af7007bda15dd16a9df61ad00cc5c7c47dc98
michael-k <michaelsure9@gmail.com>
2024-11-04 12:37:34 +0200

    Updates and repairing code

--- app/src/main/java/com/widget/testhsm/support/Interceptor.java
+++ app/src/main/java/com/widget/testhsm/support/Interceptor.java
@@ -3,58 +3,47 @@ package com.widget.testhsm.support;
 import java.util.Hashtable;
 
 public class Interceptor {
-	public  static final int INVALID_TICKET = -1;
-	public  static final int MAX_VALUE 		= Integer.MAX_VALUE;
-	private int counter_ = 0;
-	private Hashtable<Integer,Object>
-	container_ 	= new Hashtable<Integer,Object>();
-	
-	public int	PutTicket(Object object)
-	{
-		int	result = INVALID_TICKET;
-		if (object == null)
-			return	result
-		;
-		result = Generate();
-		if (result == INVALID_TICKET)
-			return	result
-		;
-		if (container_.containsKey(result))
-		{
-			result = INVALID_TICKET;
-			return	result;
-		}
-		try
-		{
-			container_.put(result, object);
-		}
-		catch(Exception exception)
-		{
-		}
+    public static final int INVALID_TICKET = -1;
+    public static final int MAX_VALUE = Integer.MAX_VALUE;
+    private int counter_ = 0;
+    private Hashtable<Integer, Object> container_ = new Hashtable<Integer, Object>();
 
-		return	result;
-	}
+    public int PutTicket(Object object) {
+        int result = INVALID_TICKET;
+        if (object == null)
+            return result;
+        result = Generate();
+        if (result == INVALID_TICKET)
+            return result;
+        if (container_.containsKey(result)) {
+            result = INVALID_TICKET;
+            return result;
+        }
+        try {
+            container_.put(result, object);
+        }
+		catch (Exception exception) {
+        }
 
-	public	Object GetTicket(int ticket)
-	{
-		Object	result	= null;
-		if (!container_.containsKey(ticket))
-			return	result;
-		result	= container_.get(ticket);
-		container_.remove(ticket);
-		return	result;
-	}
-	
-	private int Generate()
-	{
-		if (counter_ == MAX_VALUE)
-			counter_	= 0
-		;
-		return	counter_++;
-	}
-	
-	public int Size()
-	{
-		return	container_.size();
-	}
+        return result;
+    }
+
+    public Object GetTicket(int ticket) {
+        Object result = null;
+        if (!container_.containsKey(ticket))
+            return result;
+        result = container_.get(ticket);
+        container_.remove(ticket);
+        return result;
+    }
+
+    private int Generate() {
+        if (counter_ == MAX_VALUE)
+            counter_ = 0;
+        return counter_++;
+    }
+
+    public int Size() {
+        return container_.size();
+    }
 }
