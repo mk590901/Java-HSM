fab1e5b66e489e60c28334e3baef223413156e92
michael-k <michaelsure9@gmail.com>
2024-11-05 12:20:47 +0200

    Updates and repairing code

--- app/src/main/java/com/widget/testhsm/implementation/Samek_9BWrapper.java
+++ app/src/main/java/com/widget/testhsm/implementation/Samek_9BWrapper.java
@@ -4,6 +4,38 @@ import com.widget.testhsm.hsm.QEvent;
 import com.widget.testhsm.interfaces.IHsm;
 import com.widget.testhsm.interfaces.IMediator;
 
+public class Samek_9BWrapper extends IHsm {
+	private IMediator mediator_;
+	final private Samek_9BQHsmScheme entity_;
+
+	public Samek_9BWrapper(Samek_9BQHsmScheme entity, IMediator mediator) {
+		entity_ = entity;
+		mediator_ = mediator;
+		mediator_.SetHsm(this);
+	}
+	@Override
+	public IMediator Mediator() { return mediator_; }
+
+	@Override
+	public void SetMediator(IMediator mediator) { mediator_ = mediator; }
+
+	@Override
+	public void Init() {
+		entity_.init(new QEvent(Samek_9BQHsmScheme.INIT));
+	}
+	@Override
+	public void Dispatch(QEvent event) {
+		entity_.dispatch(event);
+	}
+}
+
+/*
+package com.widget.testhsm.implementation;
+
+import com.widget.testhsm.hsm.QEvent;
+import com.widget.testhsm.interfaces.IHsm;
+import com.widget.testhsm.interfaces.IMediator;
+
 public class Samek_9BWrapper extends IHsm
 {
 	private IMediator mediator_ 	= null;
@@ -40,3 +72,4 @@ public class Samek_9BWrapper extends IHsm
 	}
 
 }
+*/
